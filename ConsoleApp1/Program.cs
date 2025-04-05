58616ec439f71b07f08979bbc059ae4e6c363081
Harinov_Ivan <harinovivan410@gmail.com>
2025-04-05 16:12:13 +0300

    All tasks completed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,62 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
+        //Задание №1
+
         Console.WriteLine("Привет, мир!");
+
+        //Задание №2
+
+        Console.WriteLine("Пожалуйста, введите число!");
+        int number_user = Convert.ToInt32(Console.ReadLine());
+        if(number_user % 2 == 0)
+        {
+            Console.WriteLine("Ваше число чётное!");
+        } else {
+            Console.WriteLine("Ваше число нечётное");
+        }
+
+        //Задание №3
+
+        Console.WriteLine("Таблица умножения на 5:");
+        for(int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{i} * 5 = {i * 5}");
+        }
+
+        //Задание №4
+
+        SayHello("Иван");
+        SayHello("Дима");
+        SayHello("Андрей");
+        SayHello("Настя");
+        SayHello("Маша");
+        SayHello("Алина");
+        static void SayHello(string name)
+        {
+            Console.WriteLine($"Привет, {name}!");
+        }
+
+        //Задание №5
+
+        Person person = new Person { name = "Дима", age = 23 };
+        person.PrintInfo();
     }
 }
 
+//Задание №5
+
+class Person
+    {
+        public string name;
+        public int age;
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Имя: {name}, Возраст: {age}");
+        }
+    }
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a25158a21ed6ec75757dc63f7c9cb04d26323c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
